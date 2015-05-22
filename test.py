4b62f2b0fd85de85d693f73d87cc19f383060b4c
dkhachatrian <david.g.khachatrian@gmail.com>
2015-05-21 17:35:15 -0700

    Fixed FindPerson, Extracting ToC Data
    Person (in general) , and chapter name extraction should be fixed.
    Added new helper function, getNum, for chapter name/number extraction.
    
    TODO: - make Table of Contents extraction possible without locating dots on the previous line?
    - fix logic for dataLine now that the authors and editors are in lists

--- test.py
+++ test.py
@@ -1,3 +1,28 @@
+
+def getNum(s):
+        '''
+Takes in a string. Returns the first integer found in the string, as an int.
+        '''
+        nString = ""
+#        print("s in getNum is " + s)
+        
+        for x in range(len(s)):
+                if s[x].isdigit():
+                        nString = nString + s[x]
+##                        print("x = " + str(x))
+##                        print(" s]x] = " + s[x])
+##                        print("nString = " + nString)
+                        continue
+
+                if nString != "":
+                        break
+
+        return int(nString)
+                
+
+        
+                
+
 #person to be used to determine first and last name
 class Person():
         "A Person has a name, a role in the book (e.g. author, editor, creator), and possibly an email and/or institution."
@@ -67,13 +92,11 @@ def findPeople(s, r):
         i = 0
         pList = []
 
-        #print(words)
+##        print(words)
 
         while i + p + 1 < len(words): #going through the list
-                p = 0
+                
                 tempName = ""
-##                print("p = " + str(p))
-##                print("i = " + str(i))
 ##                print("Are words consecutive? " + str((s.find(words[i+p]) + len(words[i+p]) + len(' ') == s.find(words[i+p+1]))))
 ##                print("First word checked is "+ str(words[i+p]))
 ##                print("Index of first word checked is " + str(s.find(words[i+p])))
@@ -96,10 +119,14 @@ def findPeople(s, r):
                         person = Person(tempName, r) #make the person
                         pList.append(person) #add person to list
 
+##                print("p at the end of a cycle of the loop, before reinitialization, is " + str(p))
+                
                 i = i + p + 1 #move the index down the list to be after the consecutive words (or even if they're not consecutive)
+                p = 0
 
-                
-                
+##                print("p at the end of a cycle of the loop, after reinitialization, is " + str(p))
+##                print("i at the end of a cycle of the loop is " + str(i))                
+##                
                 ###NOTE: AS IT STANDS, will spit out site names and other consecutive capitalized strings of words as "names."
                 ## Will be handing off generated list to Deidre, who will use it to make the list of site names.
                 ## She will hand this back to me. Then I can check against these lists to ignore them instead of creating
@@ -129,104 +156,117 @@ def findPeople(s, r):
 
 
 
+#############
+########## From Table of Contents #################
+#############
 
+
+#NOTE: CURRENTLY NEEDS DOTS IN LINE PRECEDING NAMES TO FIND AUTHORS
+# CURRENTLY ONLY OBTAINS FIRST PERSON
+
+with open("Table of Contents.txt", 'r', encoding = 'utf8') as toc:
+        tocLines = toc.readlines()
+
+        #Table of Contents is organized as follows:
+        #Book title denoted by preceding "Title: "
+        #Chapter titles denoted by "Chapter X: foobar" where X is a positive integer
+        #Authors are located two lines below chapter title. There may be two authors for a chapter.
+
+        dotsFound = False #will be used to find the authors
+
+        bookTitle = ""
+        chapterTitles = []
+        editors = [] #TODO: Move "Person" class here so can use its member functions in findPerson function
+        authors = {} #dictionary will have the chapter number as the key and the various authors as their values
+
+        for line in tocLines:
+                #find the book title...
+                if bookTitle is "":
+                        x = line.find("Title: ")
+
+                        if x != -1: 
+                                x += len("Title: ")	#puts x after "Title: "
+                                bookTitle = line[x:]	#everything after the denoter
+                                continue
+
+                #find the editors...
+                if editors  == [] and ", editors" in line:
+                        x = line.find(", editors")
+                        s = line[0:x]	#just the two names in s
+                        names = findPeople(s, "editor")
+
+##                        for person in names:
+##                                person.display()
+                                
+                        editors = list(names)	#editors not associated with any particular chapter, so can just be a list
+
+
+
+
+                #get the chapter names and associate with proper numbers...
+                #chapter name will be placed in the [chapter number - 1] index...
+                if "Chapter " in line:
+                        x = line.find("Chapter ")
+                        x += len("Chapter ") #puts x right after "Chapter "
+                        i = getNum(line[x:]) #Chapter number
+                        x += len(str(i)) + len(": ") #put x right at the beginning of the chapter name
+                        y = len(line) - 1 #minus one to remove the newline character if there are no dots
+
+                        if '...' in line:
+                                y = line.index("..")
+                                
+                        
+                        s = line[x:y] #slice from after the chapter number to before the dots, if there are any)
+                                       
+
+#                        print("s = " + s)                                                              
+                        chapterTitles.insert(i-1, s) #inserts the chapter title in the (chapterNumber-1)th position in the list
+
+                #will find author line by finding the "...." line preceding it
+                #find the authors
+                if "..." in line:
+                        dotsFound = True
+                        continue #go to next line, which has just author names
+                        #########################################CHECK TO SEE WHETHER THE LINE ABOVE MESSES WITH THE FOR LOOP
+                if dotsFound:
+                        dotsFound = False
+                        tempAuthors = findPeople(line, "author")
+                        #current chapter (i.e., chapter to which the authors belong to) is the current length of the chapterTitles list
+                        #since we already added the chapter title above
+
+##                        print("For Chapter " + str(len(chapterTitles)) + ": ")
+##                        for person in tempAuthors:
+##                                
+##                                person.display()
+
+                        
+
+                        authors[str(len(chapterTitles))] = list(tempAuthors) #adds a key, chapterNumber, that has the authors (who are Persons) as their values
+                        #copy.deepcopy is not needed because the Persons in the list shouldn't be deleted
+        #done with table of contents
+
+
+#NOTE: needs three dots the line before the names to find properly
+
+
+##for person in editors:
+##        person.display()
 ##
 ##
-##
-###############
-############ From Table of Contents #################
-###############
-##
-##
-##toc = open("Table of Contents.txt", 'r')
-##
-##tocLines = toc.readlines()
-##
-###Table of Contents is organized as follows:
-###Book title denoted by preceding "Title: "
-###Chapter titles denoted by "Chapter X: foobar" where X is a positive integer
-###Authors are located two lines below chapter title. There may be two authors for a chapter.
-##
-##dotsFound = False #will be used to find the authors
-##
-##bookTitle = ""
-##chapterTitles = []
-##editors = [] #TODO: Move "Person" class here so can use its member functions in findPerson function
-##authors = {} #dictionary will have the chapter number as the key and the various authors as their values
-##
-##for line in tocLines:
-##        #find the book title...
-##        if bookTitle is "":
-##                x = line.find("Title: ")
-##
-##                if x != -1: 
-##                        x += len("Title: ")     #puts x after "Title: "
-##                        bookTitle = line[x:]    #everything after the denoter
-##                        continue
-##
-##        #find the editors...
-##        if editors  == [] and ", editors" in line:
-##                x = line.find(", editors")
-##                s = line[0:x]   #just the two names in s
-##                names = findPeople(s, "editor")
-##                editors = names #editors not associated with any particular chapter, so can just be a list
-##
-##
-##
-##
-##        #get the chapter names and associate with proper numbers...
-##        #chapter name will be placed in the [chapter number - 1] index...
-##        if "Chapter " in line:
-##                x = line.find("Chapter ")
-##                x += len("Chapter ") #puts x right after "Chapter "
-##
-##                
-##                tempNum = -1
-##                if line[x:x+2].isdigit():
-##                        tempNum = line[x:x+2]   #if chapter number is in double digits
-##                elif line[x].isdigit(): #chapter number in single digits
-##                        tempNum = line[x]
-##                        
-##                if tempNum != -1:
-##                        i = int(tempNum) #Chapter number
-##                else:   #bad!
-##                        pass
-##
-##                
-##                s = ""
-##
-##                
-##                #pass after the chapter number
-##                for word in line:
-##                        if line.index(word) < i:
-##                                continue
-##                        else:
-##                                s = line[line.index(word):] #gets rest of line. According to requested formatting, this is the chapter name
-##
-##                chapterTitles.insert(i-1, s) #inserts the chapter title in the (chapterNumber-1)th position in the list
-##
-##        #will find author line by finding the "...." line preceding it
-##        #find the authors
-##        if "..." in line:
-##                dotsFound = True
-##                continue #go to next line, which has just author names
-##                #########################################CHECK TO SEE WHETHER THE LINE ABOVE MESSES WITH THE FOR LOOP
-##        if dotsFound:
-##                dotsFound = False
-##                tempAuthors = findPeople(line, "author")
-##
-##                print(tempAuthors)
-##                break
-##                #current chapter (i.e., chapter to which the authors belong to) is the current length of the chapterTitles list
-##                #since we already added the chapter title above
-##
-##                authors[str(len(chapterTitles))] = tempAuthors #adds a key, chapterNumber, that has the authors (who are Persons) as their values
-##
-###print(bookTitle)
 ##print(authors)
-###print(editors)
-###print(chapterTitles)
 ##
-##toc.close()
-###done with table of contents
+##print('\n')
 ##
+##for i in range(1,27):
+##        x = str(i)
+##        aList = authors[x]
+##
+##        print("These are the authors found for Chapter "+ x + ": ")
+##
+##        for person in aList:
+##                person.display()
+##
+##        print('\n')
+##
+##
+##print(chapterTitles)
